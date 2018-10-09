eb4bbf9df2ecf3efea9d885eea93ef72d0c656c3
sike.chen <sike.chen@outlook.com>
2018-10-09 11:56:50 +0800

    new

--- CoreBasic/Midware/ChatServicesMidware.cs
+++ CoreBasic/Midware/ChatServicesMidware.cs
@@ -431,7 +431,7 @@ namespace CoreBasic.Midware
 				Dictionary<string, string> paramsMap_for_profle = new Dictionary<string, string>();
 				paramsMap_for_profle.Add("@suname", activeItem.name);
 				DataTable dtData = existedLoader.ExecuteSelectWithConditionsReturnDT(Global.GlobalDefines.DB_KEY_IKCODER_BASIC, Global.MapStoreProcedures.ikcoder_basic.spa_operation_relations_students, paramsMap_for_profle);
-				DataRow[] rows = dtData.Select("accepted='0'");
+				DataRow[] rows = dtData.Select("isacc='0'");
 				return Data_dbDataHelper.ActionConvertDataRowstoXMLString(rows, dtData);
 			}
 			catch (Exception err)
