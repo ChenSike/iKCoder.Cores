243f854a1e5d640e78e53fd69c8f26a346f02905
sike.chen <sike.chen@outlook.com>
2018-10-20 12:14:44 +0800

    fixed

--- CoreBasic/Controllers/Profiles_Students/Profiles_Students_SetTextInfoController.cs
+++ CoreBasic/Controllers/Profiles_Students/Profiles_Students_SetTextInfoController.cs
@@ -20,32 +20,56 @@ namespace CoreBasic.Controllers.Profiles_Students
 		[HttpGet]
 		public ContentResult actionResult(string sex, string nickname, string birthday, string state, string city, string country = "China")
 		{
-			Global.ItemAccountStudents activeItem = _appLoader.get_SessionObject(HttpContext.Session, "student_item") as Global.ItemAccountStudents;
-			Dictionary<string, string> paramsMap_for_profle = new Dictionary<string, string>();
-			paramsMap_for_profle.Add("@uid", activeItem.name);
-			DataTable dtData = _appLoader.ExecuteSelectWithConditionsReturnDT(Global.GlobalDefines.DB_KEY_IKCODER_BASIC, Global.MapStoreProcedures.ikcoder_basic.spa_operation_profile_students, paramsMap_for_profle);
-			string id = string.Empty;
-			Data_dbDataHelper.GetColumnData(dtData.Rows[0], "id", out id);
-			paramsMap_for_profle = new Dictionary<string, string>();
-			paramsMap_for_profle.Add("@id", id);
-			if (!string.IsNullOrEmpty(nickname))
-				paramsMap_for_profle.Add("@nickname", nickname);
-			if (!string.IsNullOrEmpty(birthday))
-				paramsMap_for_profle.Add("@birthday", birthday);
-			if (!string.IsNullOrEmpty(country))
-				paramsMap_for_profle.Add("@country", country);
-			if (!string.IsNullOrEmpty(state))
-				paramsMap_for_profle.Add("@state", state);
-			if (!string.IsNullOrEmpty(city))
-				paramsMap_for_profle.Add("@city", city);
-			if (_appLoader.ExecuteUpdate(Global.GlobalDefines.DB_KEY_IKCODER_BASIC, Global.MapStoreProcedures.ikcoder_basic.spa_operation_profile_students, paramsMap_for_profle))
+			try
 			{
+				string token = _appLoader.get_ClientToken(Request, "student_token");
+				Global.ItemAccountStudents activeItem = Global.LoginServices.Pull(token);
+				Dictionary<string, string> paramsMap_for_profle = new Dictionary<string, string>();
+				paramsMap_for_profle.Add("@uid", activeItem.name);
+				DataTable dtData = _appLoader.ExecuteSelectWithConditionsReturnDT(Global.GlobalDefines.DB_KEY_IKCODER_BASIC, Global.MapStoreProcedures.ikcoder_basic.spa_operation_profile_students, paramsMap_for_profle);
+				string id = string.Empty;
+				Data_dbDataHelper.GetColumnData(dtData.Rows[0], "id", out id);
+				paramsMap_for_profle = new Dictionary<string, string>();
+				paramsMap_for_profle.Add("@id", id);
+				if (!string.IsNullOrEmpty(nickname))
+				{
+					paramsMap_for_profle.Add("@nickname", nickname);
+					_appLoader.ExecuteUpdate(Global.GlobalDefines.DB_KEY_IKCODER_BASIC, Global.MapStoreProcedures.ikcoder_basic.spa_operation_profile_students, paramsMap_for_profle);
+					paramsMap_for_profle.Remove("@nickname");
+				}
+				if (!string.IsNullOrEmpty(birthday))
+				{
+					paramsMap_for_profle.Add("@birthday", birthday);
+					_appLoader.ExecuteUpdate(Global.GlobalDefines.DB_KEY_IKCODER_BASIC, Global.MapStoreProcedures.ikcoder_basic.spa_operation_profile_students, paramsMap_for_profle);
+					paramsMap_for_profle.Remove("@birthday");
+				}
+				if (!string.IsNullOrEmpty(country))
+				{
+					paramsMap_for_profle.Add("@country", country);
+					_appLoader.ExecuteUpdate(Global.GlobalDefines.DB_KEY_IKCODER_BASIC, Global.MapStoreProcedures.ikcoder_basic.spa_operation_profile_students, paramsMap_for_profle);
+					paramsMap_for_profle.Remove("@country");
+				}
+				if (!string.IsNullOrEmpty(state))
+				{
+					paramsMap_for_profle.Add("@state", state);
+					_appLoader.ExecuteUpdate(Global.GlobalDefines.DB_KEY_IKCODER_BASIC, Global.MapStoreProcedures.ikcoder_basic.spa_operation_profile_students, paramsMap_for_profle);
+					paramsMap_for_profle.Remove("@state");
+
+				}
+				if (!string.IsNullOrEmpty(city))
+				{
+					paramsMap_for_profle.Add("@city", city);
+					_appLoader.ExecuteUpdate(Global.GlobalDefines.DB_KEY_IKCODER_BASIC, Global.MapStoreProcedures.ikcoder_basic.spa_operation_profile_students, paramsMap_for_profle);
+					paramsMap_for_profle.Remove("@city");
+
+				}
 				return Content(MessageHelper.ExecuteSucessful());
 			}
-			else
+			catch
 			{
 				return Content(MessageHelper.ExecuteFalse());
 			}
+			
 		}
 	}
 }
